4edd58b9c8bdadae617e0779c901f5efe8bb4c46
Johann Egger <johann@codacy.com>
2015-02-19 10:04:35 +0200

    2 more consul -agent- endpoints that were needed: force-leave join

--- src/main/scala/consul/v1/agent/AgentRequests.scala
+++ src/main/scala/consul/v1/agent/AgentRequests.scala
@@ -3,16 +3,21 @@ package consul.v1.agent
 import consul.v1.agent.check.CheckRequests
 import consul.v1.agent.service.ServiceRequests
 import consul.v1.common.ConsulRequestBasics._
+import consul.v1.common.Types
+import consul.v1.common.Types._
 import play.api.libs.json.JsObject
+import play.api.libs.ws.WSRequestHolder
+import play.mvc.Http.Status
 
 import scala.concurrent.{ExecutionContext, Future}
 
 trait AgentRequests {
 
   def self(): Future[JsObject]
+  def join(address:String,wan:Boolean=false):Future[Boolean]
+  def `force-leave`(node:NodeId):Future[Boolean]
 
   def service: ServiceRequests
-
   def check: CheckRequests
 }
 
@@ -24,6 +29,15 @@ object AgentRequests {
       jsonRequestMaker(fullPathFor("self"), _.get())(_.validate[JsObject])
     )
 
+    def join(address: String,wan:Boolean): Future[Boolean] = responseStatusRequestMaker(
+      fullPathFor(address),
+      (r:WSRequestHolder) => (if(wan) r.withQueryString(("wan","1")) else r).get()
+    )( _ == Status.OK )
+
+    def `force-leave`(node: Types.NodeId): Future[Boolean] = responseStatusRequestMaker(
+      fullPathFor(node),_.get()
+    )( _ == Status.OK )
+
     lazy val service: ServiceRequests = ServiceRequests(currPath)
 
     lazy val check:CheckRequests = CheckRequests(currPath)
@@ -31,5 +45,6 @@ object AgentRequests {
     private lazy val currPath = s"$basePath/agent"
 
     private def fullPathFor(path: String) = s"$currPath/$path"
+
   }
 }
