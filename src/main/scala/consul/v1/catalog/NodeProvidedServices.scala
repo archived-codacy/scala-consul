bac0cc92ef4a666fd741191ce4176c1be6500f9d
Johann Egger <johann@codacy.com>
2015-03-12 14:52:32 +0200

    did not compile, sorry

--- src/main/scala/consul/v1/catalog/NodeProvidedServices.scala
+++ src/main/scala/consul/v1/catalog/NodeProvidedServices.scala
@@ -1,11 +1,11 @@
 package consul.v1.catalog
 
-import consul.v1.common.{Service, Node}
+import consul.v1.common.{Service => GService, Node}
 import consul.v1.common.Types._
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
-case class NodeProvidedServices(Node:Node,Services:Map[ServiceType,Service]){
+case class NodeProvidedServices(Node:Node,Services:Map[ServiceType,GService]){
   lazy val numServices: Int = Services.size
   lazy val serviceTypes: Set[ServiceType] = Services.values.map(_.Service).toSet
 }
@@ -13,7 +13,7 @@ object NodeProvidedServices{
 
   implicit val reads: Reads[NodeProvidedServices] = (
     ( __ \ "Node").read[Node] and
-    ( __ \ "Services").read[Map[String,Service]].map(_.map{ case (key,value) => ServiceType(key)->value })
+    ( __ \ "Services").read[Map[String,GService]].map(_.map{ case (key,value) => ServiceType(key)->value })
   )( NodeProvidedServices.apply _)
 
 }
