f41ff7378f2869e71532daf6b6a96264addd824f
Travis Brown <travisrobertbrown@gmail.com>
2016-06-08 15:52:45 -0500

    Support use of stand-alone Play WS client

--- src/main/scala/consul/v1/common/ConsulRequestBasics.scala
+++ src/main/scala/consul/v1/common/ConsulRequestBasics.scala
@@ -1,13 +1,16 @@
 package consul.v1.common
 
+import com.ning.http.client.AsyncHttpClientConfig
 import consul.v1.common.Types.DatacenterId
 import play.api.libs.json._
-import play.api.libs.ws.{WS, WSRequest, WSResponse}
-import play.api.Play.current
+import play.api.libs.ws.{WS, WSClient, WSRequest, WSResponse}
+import play.api.libs.ws.ning.NingWSClient
+import play.api.Application
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 
-class ConsulRequestBasics(token: Option[String]) {
+class ConsulRequestBasics(token: Option[String], client: WSClient) {
+  def this(token: Option[String])(implicit app: Application) = this(token, WS.client)
 
   type HttpFunc = WSRequest => Future[WSResponse]
   type RequestTransformer = WSRequest => WSRequest
@@ -40,7 +43,7 @@ class ConsulRequestBasics(token: Option[String]) {
   }
 
   private def genRequestMaker[A,B](path: String, httpFunc: HttpFunc)(responseTransformer: WSResponse => B)(body: B => A)(implicit executionContext: ExecutionContext): Future[A] = {
-    Try((withToken(token) andThen httpFunc)(WS.url(path))) match {
+    Try((withToken(token) andThen httpFunc)(client.url(path))) match {
       case Failure(exception) => Future.failed(exception)
       case Success(resultF)   => resultF.map( responseTransformer andThen body )
     }
