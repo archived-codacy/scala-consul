6b6ebcf648c12411dea6f1cc091e8b5631b90632
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2016-09-21 15:44:31 +0200

    Also exposing the ServiceAddress from the API

--- src/main/scala/consul/v1/catalog/NodeProvidingService.scala
+++ src/main/scala/consul/v1/catalog/NodeProvidingService.scala
@@ -4,7 +4,7 @@ import consul.v1.common.Types.{ServiceId, NodeId, ServiceType}
 import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
-case class NodeProvidingService(Node: NodeId, Address: String, ServiceID: ServiceId, ServiceName: ServiceType, ServiceTags:Set[String], ServicePort: Int)
+case class NodeProvidingService(Node: NodeId, Address: String, ServiceID: ServiceId, ServiceName: ServiceType, ServiceTags:Set[String], ServiceAddress: String, ServicePort: Int)
 
 object NodeProvidingService {
   implicit lazy val fmt: OFormat[NodeProvidingService] = (
@@ -13,6 +13,7 @@ object NodeProvidingService {
       (__ \ "ServiceID").format[ServiceId] and
       (__ \ "ServiceName").format[ServiceType] and
       (__ \ "ServiceTags").formatNullable[Set[String]].inmap[Set[String]](_.getOrElse(Set.empty),Option(_)) and
+      (__ \ "ServiceAddress").format[String] and
       (__ \ "ServicePort").format[Int]
     )(NodeProvidingService.apply _, unlift(NodeProvidingService.unapply _))
 }
